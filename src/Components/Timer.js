466a542b5bbf714bab3ee0f57c2a66a80bfb499e
Cole Westbrook <52761726+TheFossilDev@users.noreply.github.com>
2022-06-14 13:00:03 -0400

    Removed warnings ahead of first deployment

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useTimer } from "react-timer-hook";
 
 const Timer = ({ expiryTimestamp }) => {
-  const { seconds, minutes, hours, days, isRunning, start, pause, resume } =
+  const { seconds, minutes, isRunning, start, pause, resume } =
     useTimer({
       expiryTimestamp,
       onExpire: () => console.log("Expired!"),
