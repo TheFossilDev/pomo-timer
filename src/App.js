181d9d99f5e9cb07d43cfc5c3c116c48f16c4304
Cole Westbrook <52761726+TheFossilDev@users.noreply.github.com>
2022-07-07 13:22:40 -0400

    Basic sync to development merge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import Timer from "./Components/Timer";
 import SetTimer from "./Components/SetTimer";
@@ -6,7 +6,7 @@ import styles from "./App.module.css";
 
 const App = () => {
   // pomodor.io url?
-  
+
   // TODO: Feature list
   // 1. Set timer duration
   // 2. Work mode, short break
@@ -17,15 +17,27 @@ const App = () => {
   const [isSetting, changeIsSetting] = useState(false);
 
   const time = new Date();
-  time.setSeconds(time.getSeconds() + 1500);
+  const [workDurationOffset, setWorkDurationOffset] = useState(1500);
+  const [shortBreakDurationOffset, setShortBreakDurationOffset] =
+    useState(1500);
+  const [longBreakDurationOffset, setLongBreakDurationOffset] = useState(1500);
+  // Timer set
+  time.setSeconds(time.getSeconds() + workDurationOffset);
 
   return (
     <div className={styles.flexContainer}>
       <h3>Pomodoro Timer</h3>
-      {isSetting && <SetTimer />}
-      <Timer expiryTimestamp={time} changeIsSetting={changeIsSetting} isSetting={isSetting}/>
+      {isSetting && (
+        <SetTimer
+        />
+      )}
+      <Timer
+        expiryTimestamp={time}
+        changeIsSetting={changeIsSetting}
+        isSetting={isSetting}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/SetTimer.js
+++ src/Components/SetTimer.js
@@ -7,18 +7,37 @@ const SetTimer = props => {
     event.preventDefault();
   }
 
+  const workChangeHandler = event => {
+    console.log(`Work: ${event.target.value}`);
+    console.log(event);
+  }
+
+  const shortBreakChangeHandler = event => {
+    console.log(`Short break: ${event}`);
+  }
+
+  const longBreakChangeHandler = event => {
+    console.log(`Long break: ${event}`);
+  }
+
 
 
   return (
     <form onSubmit={submitHandler}>
       <header>
-        <h2>Timer lengths:</h2>
+        <h2>Timer durations:</h2>
       </header>
       <div>
-        <h3>Work period:</h3>
+        <label>Work minutes:</label>
+        <input type="number" onChange={workChangeHandler} value={}/>
       </div>
       <div>
-        <h3>Break period:</h3>
+        <label>Short break minutes:</label>
+        <input type="number" onChange={shortBreakChangeHandler} value={}/>
+      </div>
+      <div>
+        <label>Long break minutes:</label>
+        <input type="number" onChange={longBreakChangeHandler} value={}/>
       </div>
       <footer>
         <Button type="submit">Save</Button>
